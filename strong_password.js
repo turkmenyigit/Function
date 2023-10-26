52cc8548b45eb3a11cdd939e9f1c9c4a3017d95d
turkmenyigit <egarlyyev@yahoo.com>
2023-10-26 10:26:27 +0300

    it measures whether your password is strong enough

---
+++ strong_password.js
@@ -0,0 +1,9 @@
+function password(a){
+    if(a.length>8){
+     console.log("You choose a good password")
+    }else{
+     console.log("Not enough letters")
+    }
+}
+password("Ezizbetdir");
+password("eziz95");
