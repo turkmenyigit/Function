4ae6be07eda7defa72eea79a4a8926faa294bb26
turkmenyigit <egarlyyev@yahoo.com>
2023-10-26 10:02:09 +0300

    using multiple parameters

---
+++ family.js
@@ -0,0 +1,9 @@
+function aile (name, surname, year){
+    console.log(`${name} ${surname} ${year} doguldy`)
+}
+aile("Eziz","Garlyyev", "1995");
+aile("Meylis","Garlyyev","1988");
+aile("Myrat","Garlyyev","1956");
+aile("Jumagul","Chopanova","1958");
+aile("Gulnur","Garlyyeva","1999");
+aile("Aynur","Garlyyeva","1986");
