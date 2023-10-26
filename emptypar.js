1b25ebc77a4e5f0f37d0da693af0b5136805725c
turkmenyigit <egarlyyev@yahoo.com>
2023-10-26 10:12:05 +0300

    response if the parameter is empty

---
+++ emptypar.js
@@ -0,0 +1,8 @@
+function favFood(response){
+    if(response == undefined){
+        console.log("empty, please add your fav food")
+    }else{
+        console.log(`Your favourite food is ${response}`)
+    }
+}
+favFood();
